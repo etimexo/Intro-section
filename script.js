4b59c08dd7e4d136192415a978f3d1977f26a7cc
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-06-28 00:44:28 +0100

    Edit files

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+// const toggle = document.querySelector("#toggler")
+// const navMenu = document.querySelector(".nav-menu")
+// const togglerImage = toggle.querySelector("img")
+// const toggleOpen = "./images/icon-menu.svg"
+// const toggleClose = "./images/icon-close-menu.svg"
+// const features = document.querySelector("#nav-item1")
+// const dropdown1 = document.querySelector(".dropdown1")
+// const company = document.querySelector("#nav-item2")
+// const dropdown2 = document.querySelector(".dropdown2")
+// let isDropdown1Hovered = false
+// let isDropdown2Hovered = false
+// features.addEventListener("mouseenter", function () {
+//     dropdown1.classList.add("active")
+//   })
+  
+//   features.addEventListener("mouseleave", function () {
+//         dropdown1.classList.remove("active")
+//   })
+//   features.addEventListener("mouseleave", function () {
+//     setTimeout(() => {
+//       if (!isDropdown1Hovered) {
+//         dropdown1.classList.remove("active")
+//       }
+//     }, 200)
+//   })
+  
+//   dropdown1.addEventListener("mouseenter", function () {
+//     isDropdown1Hovered = true
+//   })
+//   dropdown1.addEventListener("mouseleave", function () {
+//     isDropdown1Hovered = false
+//   })
+//   company.addEventListener("mouseenter", function () {
+//     dropdown2.classList.add("active")
+//   })
+  
+//   company.addEventListener("mouseleave", function () {
+//     dropdown2.classList.remove("active")
+//   })
+// toggle.addEventListener("click", () => {
+//     toggle.classList.toggle("active")
+//     navMenu.classList.toggle("active")
+//     if ( toggle.classList.contains("active")) {
+//         togglerImage.src = toggleClose
+//     } else {
+//         togglerImage.src = toggleOpen
+//     }
+// })
+
 const toggle = document.querySelector("#toggler")
 const navMenu = document.querySelector(".nav-menu")
 const togglerImage = toggle.querySelector("img")
@@ -9,94 +58,41 @@ const company = document.querySelector(".company")
 const dropdown2 = document.querySelector(".dropdown2")
 let isDropdown1Hovered = false
 let isDropdown2Hovered = false
+
 features.addEventListener("mouseenter", function () {
-    dropdown1.classList.add("active")
-  })
-  
-  features.addEventListener("mouseleave", function () {
-        dropdown1.classList.remove("active")
-  })
-  features.addEventListener("mouseleave", function () {
-    if (!isDropdown1Hovered) {
-      dropdown1.classList.remove("active");
-    }
-  });
-  
-  dropdown1.addEventListener("mouseenter", function () {
-    isDropdown1Hovered = true;
-  });
-  dropdown1.addEventListener("mouseleave", function () {
-    isDropdown1Hovered = false;
-    dropdown1.classList.remove("active");
-  });
-  
-  
-  company.addEventListener("mouseenter", function () {
-    dropdown2.classList.add("active")
-  })
-  
-  company.addEventListener("mouseleave", function () {
-    dropdown2.classList.remove("active")
-  })
-toggle.addEventListener("click", () => {
-    toggle.classList.toggle("active")
-    navMenu.classList.toggle("active")
-    if ( toggle.classList.contains("active")) {
-        togglerImage.src = toggleClose
-    } else {
-        togglerImage.src = toggleOpen
-    }
+  dropdown1.classList.add("active")
 })
 
-// const toggle = document.querySelector(".toggler");
-// const navMenu = document.querySelector(".nav-menu");
-// const togglerImage = toggle.querySelector("img");
-// const toggleOpen = "./images/icon-menu.svg";
-// const toggleClose = "./images/icon-close-menu.svg";
-// const features = document.querySelector(".features");
-// const dropdown1 = document.querySelector(".dropdown1");
-// const company = document.querySelector(".company");
-// const dropdown2 = document.querySelector(".dropdown2");
-// let isDropdown1Hovered = false;
-// let isDropdown2Hovered = false;
+features.addEventListener("mouseleave", function () {
+  setTimeout(() => {
+    if (!isDropdown1Hovered) {
+      dropdown1.classList.remove("active")
+    }
+  }, 100)
+})
 
-// features.addEventListener("mouseenter", function () {
-//   dropdown1.classList.add("active");
-// });
+dropdown1.addEventListener("mouseenter", function () {
+  isDropdown1Hovered = true
+})
 
-// features.addEventListener("mouseleave", function () {
-//   if (!isDropdown1Hovered) {
-//     dropdown1.classList.remove("active");
-//   }
-// });
+dropdown1.addEventListener("mouseleave", function () {
+  isDropdown1Hovered = false
+})
 
-// dropdown1.addEventListener("mouseenter", function () {
-//   isDropdown1Hovered = true;
-// });
+company.addEventListener("mouseenter", function () {
+  dropdown2.classList.add("active")
+})
 
-// dropdown1.addEventListener("mouseleave", function () {
-//   isDropdown1Hovered = false;
-//   setTimeout(() => {
-//     if (!isDropdown1Hovered) {
-//       dropdown1.classList.remove("active");
-//     }
-//   }, 200); // Add a slight delay before removing the "active" class
-// });
+company.addEventListener("mouseleave", function () {
+  dropdown2.classList.remove("active")
+})
 
-// company.addEventListener("mouseenter", function () {
-//   dropdown2.classList.add("active");
-// });
-
-// company.addEventListener("mouseleave", function () {
-//   dropdown2.classList.remove("active");
-// });
-
-// toggle.addEventListener("click", () => {
-//   toggle.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-//   if (toggle.classList.contains("active")) {
-//     togglerImage.src = toggleClose;
-//   } else {
-//     togglerImage.src = toggleOpen;
-//   }
-// });
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active")
+  navMenu.classList.toggle("active")
+  if (toggle.classList.contains("active")) {
+    togglerImage.src = toggleClose
+  } else {
+    togglerImage.src = toggleOpen
+  }
+})
